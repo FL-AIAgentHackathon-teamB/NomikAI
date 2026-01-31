fb5b99d6cfd699ba016a2a63c01c66772715e417
Kensuke Nozaki <kensuke.nozaki@flinters.co.jp>
2026-01-31 19:13:02 +0900

    一旦terraformディレクトリを空に

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,33 +8,3 @@ variable "region" {
   type        = string
   default     = "asia-northeast1"
 }
-
-variable "backend_service_name" {
-  description = "Backend API service name"
-  type        = string
-  default     = "nomikai-backend"
-}
-
-variable "frontend_service_name" {
-  description = "Frontend service name"
-  type        = string
-  default     = "nomikai-frontend"
-}
-
-variable "gemini_api_key" {
-  description = "Gemini API Key"
-  type        = string
-  sensitive   = true
-}
-
-variable "min_instances" {
-  description = "Minimum number of instances"
-  type        = number
-  default     = 0
-}
-
-variable "max_instances" {
-  description = "Maximum number of instances"
-  type        = number
-  default     = 10
-}
